ba54265fb9bfe7f1cda9da845332a89d3730d5a8
oom- <11071671+oom-@users.noreply.github.com>
2019-08-31 02:30:14 +0200

    Update

--- InjectionLib/Source.cpp
+++ InjectionLib/Source.cpp
@@ -8,27 +8,11 @@
 #include <list>
 #include <Psapi.h>
 
-/*
-Life:
-Undertale.exe + 0039A148 -> val
-val + 18 -> val2
-val2 + 2F8 -> addr
-addr -> life
-
-MaxLife:
-Undertale.exe + 0039B224 -> val
-val + 3F4 -> val2
-val2 + 298 -> val3
-val3 + 164 -> val4
-val4 + 18 -> val5
-val5 + 2C8 -> addr
-addr -> lifemax
-*/
-
 #define KEY_DOWN 0
 #define KEY_UP 1
 #define VK_DOLLAR 186
 #define HAVE_GUARD(value)((value / PAGE_GUARD) >= 1 && 2 > (value / PAGE_GUARD))
+
 LPVOID baseAddr;
 HWND cWindow;
 
@@ -56,7 +40,6 @@ void printLogs(std::string s)
 /*Cheat*/
 void TurnFullLife()
 {
-	
 	LPVOID addr = baseAddr;
 		
 	//Read max life
@@ -75,7 +58,7 @@ void TurnFullLife()
 	addr = (LPVOID)((DWORD)addr + 0x1D0);
 	writeMemory<double>(addr, maxlife);
 	
-	printLogs("Done");
+	//printLogs("Done");
 }
 
 /*Key detection and main*/
@@ -116,26 +99,25 @@ DWORD WINAPI hookKeys(LPVOID lparam)
 
 LPVOID ScanCurrentProcessMemoryFromPattern(std::string pattern) //get pattern in memory 
 {
-	printLogs("Lancement du thread");
+	//printLogs("Lancement du thread");
 	std::list<std::string> dividedPattern;
 	size_t pos = 0;
 	std::string token;
 
-	printLogs("Pattern: " + pattern);
+	//printLogs("Pattern: " + pattern);
 	while ((pos = pattern.find(" ")) != std::string::npos) { //get all byte in string
 		token = pattern.substr(0, pos);
 		dividedPattern.push_back(token);
 		pattern.erase(0, pos + 1);
 	}
-	printLogs("Pattern parsé");
 
+	//printLogs("Pattern parsé");
 	MEMORY_BASIC_INFORMATION mbi; //scan memory
 	DWORD ptr = NULL;
 	int index = 0;
 	int number = 0;
 	while (VirtualQuery((LPVOID)ptr, &mbi, sizeof(mbi)) != 0)
 	{
-
 		number++;
 		if ((DWORD)ptr != 0x0078D000)
 		{
@@ -144,7 +126,7 @@ LPVOID ScanCurrentProcessMemoryFromPattern(std::string pattern) //get pattern in
 		}
 		if (mbi.State != MEM_RESERVE && mbi.Protect != PAGE_NOACCESS && !(HAVE_GUARD(mbi.Protect)) && (mbi.AllocationProtect == PAGE_EXECUTE_READWRITE || mbi.AllocationProtect == PAGE_READWRITE))
 		{
-			printLogs("Nouvel plage");
+			/*printLogs("Nouvel plage");
 			std::stringstream stream;
 			stream << "--Numéro" << std::dec << number << std::endl;
 			stream << "--AllocationBase: 0x" << std::hex << mbi.AllocationBase << std::endl;
@@ -152,8 +134,7 @@ LPVOID ScanCurrentProcessMemoryFromPattern(std::string pattern) //get pattern in
 			stream << "--State: 0x" << std::hex << mbi.State << std::endl;
 			stream << "--Protect: 0x" << std::hex << mbi.Protect << std::endl;
 			stream << "--AllocationProtect: 0x" << std::hex << mbi.AllocationProtect << std::endl;
-
-			printLogs(stream.str());
+			printLogs(stream.str());*/
 			SIZE_T i = 0;
 			SIZE_T scanindex = 0;
 			BYTE currval = 0;
@@ -190,35 +171,34 @@ LPVOID ScanCurrentProcessMemoryFromPattern(std::string pattern) //get pattern in
 		ptr = (DWORD)((DWORD)mbi.BaseAddress + mbi.RegionSize);
 	}
 	return 0;
-} //useless but good for future
+} //useless but good too keep some code online
 
 LPVOID ScanMainModuleFromPattern(std::string pattern)
 {
-	printLogs("Lancement scan main module");
+	//printLogs("Lancement scan main module");
 	std::list<std::string> dividedPattern;
 	size_t pos = 0;
 	std::string token;
 
-	printLogs("Pattern: " + pattern);
+	//printLogs("Pattern: " + pattern);
 	while ((pos = pattern.find(" ")) != std::string::npos) { //get all byte in string
 		token = pattern.substr(0, pos);
 		dividedPattern.push_back(token);
 		pattern.erase(0, pos + 1);
 	}
-	printLogs("Pattern parsé");
+	//printLogs("Pattern parsé");
 
 	HMODULE base = GetModuleHandle(NULL);
 	HANDLE proc = GetCurrentProcess();
 	MODULEINFO  lpmodinfo;
-	GetModuleInformation(proc, base, &lpmodinfo, sizeof(lpmodinfo));
-
-
 	SIZE_T scanindex = 0;
 	BYTE currval = 0;
 	BYTE byte = 0;
 	std::list<std::string>::iterator it;
 	SIZE_T sizescan = dividedPattern.size();
 	DWORD i = 0;
+
+	GetModuleInformation(proc, base, &lpmodinfo, sizeof(lpmodinfo)); //get module size
 	for (i = 0; i < lpmodinfo.SizeOfImage; i++)
 	{
 		currval = *((BYTE*)((DWORD)lpmodinfo.lpBaseOfDll + i));
@@ -245,7 +225,6 @@ LPVOID ScanMainModuleFromPattern(std::string pattern)
 			}
 		}
 	}
-
 	return 0;
 }
 
@@ -253,9 +232,14 @@ DWORD WINAPI runDll(LPVOID lparam)
 {
 	HANDLE thread;
 
-	printLogs("Lancement du Thread (scan)");
-	baseAddr = ScanMainModuleFromPattern("89 5E * 8B 0D * * * * 8D 87");
-	printLogs(baseAddr == 0 ? "PAS TROUVER" : "TROUVEYYYYYY");
+	//printLogs("Lancement du Thread (scan)");
+	while (baseAddr == 0)
+	{
+		baseAddr = ScanMainModuleFromPattern("89 5E * 8B 0D * * * * 8D 87");
+		if (baseAddr == 0)
+			Sleep(2000); //wait 2 sec before scan again 
+	}
+	//printLogs(baseAddr == 0 ? "Not found" : "found");
 	baseAddr = (LPVOID)((DWORD)baseAddr + 5);
 	thread = CreateThread(NULL, 0, &hookKeys, NULL, 0, NULL); //runkey detection thread
 	CloseHandle(thread);
@@ -268,7 +252,8 @@ BOOL WINAPI DllMain(HINSTANCE hInst, DWORD reason, LPVOID)
 
 	if (reason == DLL_PROCESS_ATTACH)
 	{
-		printLogs("Lancement du Thread (DLL)");
+		//printLogs("DLL INJECTED :D");
+		baseAddr = 0;
 		cWindow = FindWindowA(NULL, "UNDERTALE");
 		thread = CreateThread(NULL, 0, &runDll, NULL, 0, NULL); //run memoryscan
 		CloseHandle(thread);
